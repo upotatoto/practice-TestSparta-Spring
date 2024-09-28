21505de552b88a63fc96b45bbbb4d8cf5a279fa7
whlsls3377 <whlsls3377@naver.com>
2024-09-28 21:48:57 +0900

    내일배움캠프 spring init first commit

---
+++ src/main/java/com/sparta/spartamvc/SpartaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartamvc/SpartaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
